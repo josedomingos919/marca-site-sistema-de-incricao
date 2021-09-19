048777985403f3eaf34d5b7e415dc4aee070e4e7
josedomingos919 <josedomingos919@gmail.com>
2021-09-19 10:20:59 +0100

    start login screen

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -6,8 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="assets/css/main.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/login.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    Teste
+
+    <div class="div-sec-left"></div>
+    <section class="sec-container-div">
+        <div class="container-login">
+            <div class="left-login">1</div>
+            <div class="right-login">2</div>
+        </div>
+    </section>
+
 </body>
 </html>
